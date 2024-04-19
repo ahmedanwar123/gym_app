8e1d9514e2bea02210111c919cc4f1c817cf348f
ahmedanwar123 <ahmed.anwar2003@gmail.com>
2024-04-19 18:59:36 +0200

    modifications to the themes

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const Color bgColor = Color(0xFF424242);
 const Color secondaryColor = Colors.black45;
 const Color textColor = Colors.white;
-const Color buttonColor = Colors.amber;
-
+const Color buttonColor = Color(0xFFFF8F00);
+// to use it in your page import the file
 // use roboto fonts for the app , it doesn't need to be specified in the themes
