32af2455f06b1b62906f69a6ffe63f2ba04f2ed2
rezk1834 <moustafarezk@ejust.edu.eg>
2024-04-24 02:58:01 +0200

    Added the SmoothPageIndicator feature and changed the position of the third page text.

--- lib/screens/onboarding_1.dart
+++ lib/screens/onboarding_1.dart
@@ -1,77 +1,109 @@
-import 'package:app/screens/onboarding_2.dart';
-import 'package:app/screens/onboarding_3.dart';
 import 'package:flutter/material.dart';
 import 'package:app/shared/theme.dart';
-// Import Screen_3
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'Onboarding_2.dart';
+import 'Onboarding_3.dart';
 
-class Onboarding_1 extends StatelessWidget {
-  const Onboarding_1({super.key});
+class Onboarding_1 extends StatefulWidget {
+  const Onboarding_1({Key? key}) : super(key: key);
+
+  @override
+  _Onboarding_1State createState() => _Onboarding_1State();
+}
+
+class _Onboarding_1State extends State<Onboarding_1> {
+  final PageController _controller = PageController();
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
-      body: PageView(
-        scrollDirection: Axis.horizontal,
+      body: Stack(
         children: [
-          Scaffold(
-            backgroundColor: bgColor,
-            body: Stack(
-              children: [
-                Image.asset(
-                  'assets/images/Un1.png',
-                  fit: BoxFit.cover, // Cover the entire screen
-                  width: MediaQuery.of(context)
-                      .size
-                      .width, // Set width to screen width
-                  height: MediaQuery.of(context)
-                      .size
-                      .height, // Set height to screen height
-                ),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: MediaQuery.of(context).size.height *
-                      0.2, // Adjust this value to move the text higher
-                  child: const Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            'WITH YOUR AI COACH,',
-                            style: TextStyle(
-                              color: textColor,
-                              fontSize: 22,
-                              fontFamily: 'Roboto',
-                            ),
-                            textAlign: TextAlign.center, // Justify the text
+          PageView(
+            controller: _controller,
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (int page) {
+              setState(() {
+                _currentPage = page;
+              });
+            },
+            children: [
+              Scaffold(
+                backgroundColor: bgColor,
+                body: Stack(
+                  children: [
+                    Image.asset(
+                      'assets/images/Un1.png',
+                      fit: BoxFit.cover,
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                    ),
+                    Positioned(
+                      left: 0,
+                      right: 0,
+                      bottom: MediaQuery.of(context).size.height * 0.2,
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Text(
+                                'WITH YOUR AI COACH,',
+                                style: TextStyle(
+                                  color: textColor,
+                                  fontSize: 22,
+                                  fontFamily: 'Roboto',
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(height: 8),
+                              const Text(
+                                'START YOUR JOURNEY',
+                                style: TextStyle(
+                                  color: textColor,
+                                  fontSize: 22,
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
                           ),
-                          SizedBox(
-                              height:
-                                  8), // Add some space between the two lines
-                          Text(
-                            'START YOUR JOURNEY',
-                            style: TextStyle(
-                              color: textColor,
-                              fontSize: 22,
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.bold, // Make the text bold
-                            ),
-                            textAlign: TextAlign.center, // Justify the text
-                          ),
-                        ],
+                        ),
                       ),
                     ),
+                  ],
+                ),
+              ),
+              Onboarding_2(),
+              Onboarding_3(),
+            ],
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 20.0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Center(
+                child: SmoothPageIndicator(
+                  controller: _controller,
+                  count: 3, // Adjust count according to the number of pages
+                  effect: ExpandingDotsEffect(
+                    dotHeight: 12, // Adjust the height of the dots
+                    dotWidth: 24, // Adjust the width of the dots
+                    dotColor: unswippedpagecolor, // Set color for unswiped dots
+                    activeDotColor: swippedpagecolor, // Set color for swiped dot
+                    expansionFactor: 2, // Adjust the expansion factor for the active dot
                   ),
                 ),
-              ],
+              ),
             ),
           ),
-          Onboarding_2(),
-          Onboarding_3(),
         ],
       ),
     );
